c9c82c9fe9c8b260c7b5c31f22d945ae0512265f
root <root@homeassistant.local.hass.io>
2023-10-01 22:16:52 +0100

    📁 files on Oct 01, 2023 - 10:16 PM

--- esphome/.esphome/build/tagreader-98db53/src/esphome/core/version.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "esphome/core/macros.h"
-#define ESPHOME_VERSION "2022.2.4"
-#define ESPHOME_VERSION_CODE VERSION_CODE(2022, 2, 4)
--- esphome/.esphome/build/tagreader/src/esphome/core/version.h
+++
@@ -1,4 +0,0 @@
-#pragma once
-#include "esphome/core/macros.h"
-#define ESPHOME_VERSION "2023.6.3"
-#define ESPHOME_VERSION_CODE VERSION_CODE(2023, 6, 3)
