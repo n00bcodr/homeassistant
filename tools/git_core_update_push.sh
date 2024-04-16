7c947e9bd1cb49b34e6d2bd05a452034b15cf2de
root <root@homeassistant.local.hass.io>
2024-04-16 17:18:10 +0000

    📁 files on Apr 16, 2024 - 5:18 PM

--- tools/git_core_update_push.sh
+++ tools/git_core_update_push.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /config
-git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
+#git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
 ver=$(<.HA_VERSION)
 git add .HA_VERSION -f
 git commit -m "📦⬆ Core Version Update to $ver"
--- tools/ha_gitpush.sh
+++ tools/ha_gitpush.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd /config
-git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
+#git config core.sshCommand 'ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no -i /config/.ssh/id_rsa -F /dev/null'
 git add .
 git commit -m "$@"
 git status
